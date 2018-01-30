2f431362c84a11a3dbe5a8a86cd6ddf41fbdbbfd
Abhijit Kar <ak24284@gmail.com>
2018-01-31 00:20:41 +0530

    PUT User Implemented & Omit Empty Property Added to JSON

--- apis/users.go
+++ apis/users.go
@@ -16,15 +16,15 @@ var projectID = "chrome-setup-158308"
 type User struct {
 	// Auto Generated
 	// Read Only: true
-	ID *datastore.Key `datastore:"__key__" json:"id"`
+	ID *datastore.Key `datastore:"__key__" json:"id,omitempty"`
 
 	// From where to inherit
 	// Required: true
-	ParentID string `datastore:"-" json:"parent_id"`
+	ParentID string `datastore:"" json:"parent_id,omitempty"`
 
 	// First Name of the User
 	// Required: true
-	Firstname string `datastore:"firstname" json:"firstname"`
+	Firstname string `datastore:"firstname,omitempty" json:"firstname,omitempty"`
 
 	// Middle Name of the User
 	Middlename string `datastore:"middlename,omitempty" json:"middlename,omitempty"`
@@ -33,14 +33,13 @@ type User struct {
 	Lastname string `datastore:"lastname,omitempty" json:"lastname,omitempty"`
 
 	// Validated Email Ids
-	EmailIds []string `datastore:"email_ids" json:"email_ids"`
+	EmailIds []string `datastore:"email_ids,omitempty" json:"email_ids,omitempty"`
 
 	// 10 Digit Unformatted Phone Numbers
-	PhoneNumbers []string `datastore:"phone_numbers" json:"phone_numbers"`
+	PhoneNumbers []string `datastore:"phone_numbers,omitempty" json:"phone_numbers,omitempty"`
 
 	// Date of Birth in UTC Zulu Format
-	// Required: true
-	Dob time.Time `datastore:"dob" json:"dob"`
+	Dob time.Time `datastore:"dob,omitempty" json:"dob,omitempty"`
 
 	// Role of the User
 	Role string `datastore:"role,omitempty" json:"role,omitempty"`
@@ -191,12 +190,56 @@ func GetUser(w http.ResponseWriter, r *http.Request) {
 	w.Write(json)
 }
 
-func PatchUser(w http.ResponseWriter, r *http.Request) {
-	w.WriteHeader(http.StatusOK)
-}
-
 func UpdateUser(w http.ResponseWriter, r *http.Request) {
+	ctx := appengine.NewContext(r)
+	params := mux.Vars(r)
+
+	client, err := datastore.NewClient(ctx, projectID)
+	if err != nil {
+		http.Error(w, "Failed to establish connection with Database: "+err.Error(), http.StatusServiceUnavailable)
+		return
+	}
+
+	key, err := datastore.DecodeKey(params["id"])
+	if err != nil {
+		http.Error(w, "Unable to Decode ID: "+err.Error(), http.StatusBadRequest)
+		return
+	}
+	key.Namespace = "NeverLand"
+
+	b, _ := ioutil.ReadAll(r.Body)
+
+	var expectedUser = new(User)
+
+	err = json.Unmarshal(b, expectedUser)
+	if err != nil {
+		http.Error(w, "Unable to parse Request BODY: "+err.Error(), http.StatusUnprocessableEntity)
+		return
+	}
+
+	var user User
+
+	_, err = client.RunInTransaction(ctx, func(tx *datastore.Transaction) error {
+		if err := tx.Get(key, &user); err != nil && err != datastore.ErrNoSuchEntity {
+			return err
+		}
+		user = *expectedUser
+		_, err := tx.Put(key, &user)
+		return err
+	})
+	if err != nil {
+		http.Error(w, "Failed to Update: "+err.Error(), http.StatusExpectationFailed)
+		return
+	}
+
+	json, err := json.Marshal(user)
+	if err != nil {
+		http.Error(w, "Unable to convert response to JSON: "+err.Error(), http.StatusInternalServerError)
+		return
+	}
+
 	w.WriteHeader(http.StatusOK)
+	w.Write(json)
 }
 
 func UploadProfileImage(w http.ResponseWriter, r *http.Request) {
